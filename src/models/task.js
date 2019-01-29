abadf6b68bfd3675da1c680b071e66356c5dcac6
lu4ezar <misha-shisha@mail.ru>
2019-01-29 20:34:01 +0700

    prettier

--- src/models/task.js
+++ src/models/task.js
@@ -1,26 +1,26 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const TaskSchema = new mongoose.Schema(
-  {
-    owner: String,
-    task: {
-      type: String,
-      required: true,
-    },
-    priority: {
-      type: String,
-      default: 'normal',
-      enum: ['low', 'normal', 'high'],
-    },
-    status: {
-      type: String,
-      default: 'new',
-      enum: ['new', 'in work', 'done'],
-    },
-  },
-  {
-    timestamps: true,
-  }
-)
+	{
+		owner: String,
+		task: {
+			type: String,
+			required: true
+		},
+		priority: {
+			type: String,
+			default: 'normal',
+			enum: ['low', 'normal', 'high']
+		},
+		status: {
+			type: String,
+			default: 'new',
+			enum: ['new', 'in work', 'done']
+		}
+	},
+	{
+		timestamps: true
+	}
+);
 
-module.exports = mongoose.model('Task', TaskSchema)
+module.exports = mongoose.model('Task', TaskSchema);
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,85 +1,85 @@
 //@ts-check
-const Router = require('koa-router')
-const router = new Router()
-const passport = require('koa-passport')
-const Task = require('../api/task')
-const User = require('../api/user')
+const Router = require('koa-router');
+const router = new Router();
+const passport = require('koa-passport');
+const Task = require('../api/task');
+const User = require('../api/user');
 
 // common routes
 
 router
-  .get('/login', async ctx => ctx.render('/login'))
-  .post(
-    '/login',
-    passport.authenticate('local', {
-      successRedirect: '/',
-      failureRedirect: '/login',
-      successFlash: 'Welcome back!',
-      failureFlash: true,
-    })
-  )
+	.get('/login', async ctx => ctx.render('/login'))
+	.post(
+		'/login',
+		passport.authenticate('local', {
+			successRedirect: '/',
+			failureRedirect: '/login',
+			successFlash: 'Welcome back!',
+			failureFlash: true
+		})
+	);
 
 router
-  .get('/register', async ctx => ctx.render('register'))
-  .post('/register', async ctx => {
-    try {
-      const user = await User.createUser(ctx.request.body)
-      ctx.login(user)
-      ctx.flash('success', `Welcome new user ${user.username}!`)
-      ctx.redirect('/')
-    } catch (err) {
-      ctx.flash('error', err.message)
-      ctx.redirect('/register')
-    }
-  })
+	.get('/register', async ctx => ctx.render('register'))
+	.post('/register', async ctx => {
+		try {
+			const user = await User.createUser(ctx.request.body);
+			ctx.login(user);
+			ctx.flash('success', `Welcome new user ${user.username}!`);
+			ctx.redirect('/');
+		} catch (err) {
+			ctx.flash('error', err.message);
+			ctx.redirect('/register');
+		}
+	});
 
-router.get('/favicon.ico', ctx => (ctx.status = 204))
+router.get('/favicon.ico', ctx => (ctx.status = 204));
 
 // protected routes
 
 async function restrictAccess(ctx, next) {
-  if (!ctx.isAuthenticated()) {
-    return ctx.redirect('/login')
-  }
-  return next()
+	if (!ctx.isAuthenticated()) {
+		return ctx.redirect('/login');
+	}
+	return next();
 }
 
-router.use(restrictAccess)
+router.use(restrictAccess);
 
 router
-  .get('/', async ctx => {
-    const tasks = await Task.getTasks(ctx)
-    const { user } = ctx.state
-    return ctx.render('/index', {
-      user: user,
-      title: 'Tasks',
-      tasks: tasks,
-    })
-  })
-  .get('/add', async ctx => ctx.render('add'))
-  .post('/add', async ctx => {
-    try {
-      const task = await Task.createTask(
-        ctx.state.user.username,
-        ctx.request.body.task
-      )
-      ctx.flash('success', `${task.task} was added!`)
-      ctx.redirect('/')
-    } catch (err) {
-      ctx.flash('error', err.message)
-      ctx.redirect('/add')
-    }
-  })
-  .get('/logout', async ctx => {
-    ctx.logout()
-    ctx.redirect('/')
-  })
+	.get('/', async ctx => {
+		const tasks = await Task.getTasks(ctx);
+		const { user } = ctx.state;
+		return ctx.render('/index', {
+			user: user,
+			title: 'Tasks',
+			tasks: tasks
+		});
+	})
+	.get('/add', async ctx => ctx.render('add'))
+	.post('/add', async ctx => {
+		try {
+			const task = await Task.createTask(
+				ctx.state.user.username,
+				ctx.request.body.task
+			);
+			ctx.flash('success', `${task.task} was added!`);
+			ctx.redirect('/');
+		} catch (err) {
+			ctx.flash('error', err.message);
+			ctx.redirect('/add');
+		}
+	})
+	.get('/logout', async ctx => {
+		ctx.logout();
+		ctx.redirect('/');
+	});
 
 module.exports = {
-  routes() {
-    return router.routes()
-  },
-  allowedMethods() {
-    return router.allowedMethods()
-  },
-}
+	routes() {
+		return router.routes();
+	},
+	allowedMethods() {
+		return router.allowedMethods();
+	}
+};
